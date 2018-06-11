8509777b43d085bc4b9336efc5f01e9a8095a4b1
Unknown <ricardfosserda@gmail.com>
2018-06-11 04:40:59 +0200

    set user notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -49,6 +49,11 @@ class NotificationsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    def set_user
+      @user = User.find_by!(user_auth_token: params[:user_auth_token])
+    end
+
     def set_notification
       @notification = Notification.find(params[:id])
     end
